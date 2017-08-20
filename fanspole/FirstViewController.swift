6c1dac9edc1977d0a22dd6acf69b5ef9d3427382
YOUR NAME <hardikg23@gmail.com>
2017-08-20 16:44:35 +0530

    Initial Commit

---
+++ fanspole/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  fanspole
+//
+//  Created by Hardik on 20/08/17.
+//  Copyright © 2017 Fanspole. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
